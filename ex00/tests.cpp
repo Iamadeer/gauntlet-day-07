49134624fb4eef6559240c332a6a6d84952269d0
Lam Jun Han <junhanlam0@gmail.com>
2026-08-15 00:03:43 +0800

    red: Check for has function can find the item

--- ex00/tests.cpp
+++ ex00/tests.cpp
@@ -10,3 +10,10 @@ TEST_CASE("Adding an item increases the counter"){
     CHECK(inv.count() == 1);
 }
 
+TEST_CASE("Check for has function can find the item"){
+    Inventory inv;
+    inv.add("sword");
+    CHECK(inv.has("sword") == true);
+    CHECK(inv.has("shield") == false);
+}
+
