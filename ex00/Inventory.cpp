197e192c3d741557704bd200e975ba39fe254814
Lam Jun Han <junhanlam0@gmail.com>
2026-08-15 00:04:23 +0800

    green: Check for has function can find the item

--- ex00/Inventory.cpp
+++ ex00/Inventory.cpp
@@ -8,7 +8,12 @@ void Inventory::add(const std::string& item){
 void Inventory::remove(const std::string& /*item*/){
 
 };   
-bool Inventory::has(const std::string& /*item*/) const{
+bool Inventory::has(const std::string& item) const{
+    for (const std::string& inventory : m_inventory){
+        if (inventory == item) {
+            return true;
+        }
+    }
     return false;
 };
 int  Inventory::count() const{
