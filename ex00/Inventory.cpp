15426faa8b37c155d7399f2c98f9cad92e7743f2
Lam Jun Han <junhanlam0@gmail.com>
2026-08-15 00:05:16 +0800

    green: Remove should reduce the counter and has cannot find the deleted item

--- ex00/Inventory.cpp
+++ ex00/Inventory.cpp
@@ -5,7 +5,14 @@ void Inventory::add(const std::string& item){
     m_inventory.push_back(item);
     m_count ++;
 };
-void Inventory::remove(const std::string& /*item*/){
+void Inventory::remove(const std::string& item){
+    for (auto it = m_inventory.begin(); it != m_inventory.end(); it++){
+        if (*it == item){
+            m_inventory.erase(it);
+            m_count -= 1;
+            return;
+        }
+    }
 
 };   
 bool Inventory::has(const std::string& item) const{
