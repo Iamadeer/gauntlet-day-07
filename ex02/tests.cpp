c8280ff40759b35a5a3e4b8494683a775004a4cc
Lam Jun Han <junhanlam0@gmail.com>
2026-08-15 16:44:08 +0800

    Update

--- ex02/tests.cpp
+++ ex02/tests.cpp
@@ -7,13 +7,16 @@ class FakeRandom : public RandomSource{
 public:
     int next(int) override{return m_value;};
 
+    void setRng(int num) { m_value = num;};
 
+
+private:
     int m_value;
 };
 
 TEST_CASE("rng value 0 should give 1"){
     FakeRandom rng;
-    rng.m_value = 0;
+    rng.setRng(0);
     Dice dice(rng);
 
     CHECK(dice.roll() == 1);
@@ -22,7 +25,7 @@ TEST_CASE("rng value 0 should give 1"){
 
 TEST_CASE("rng value 5 should give 6"){
     FakeRandom rng;
-    rng.m_value = 5;
+    rng.setRng(5);
     Dice dice(rng);
 
     CHECK(dice.roll() == 6);
